0037797e06d5f484c6490b97b9d6ec86d2219d6d
Andreas Jocksch <andreas.jocksch@cscs.ch>
2021-11-19 10:51:47 +0100

    test

--- include/core/cost_simulation.h
+++ include/core/cost_simulation.h
@@ -29,6 +29,11 @@ int ext_mpi_cost_simulation(int count, int type_size, int comm_size_row,
                             int my_cores_per_node_column, int comm_size_rowb,
                             int comm_rank_row, int simulate);
 
+int ext_mpi_allreduce_simulate(int count, int type_size,
+                               int comm_size_row, int my_cores_per_node_row,
+                               int comm_size_column,
+                               int my_cores_per_node_column);
+
 #ifdef __cplusplus
 }
 #endif
