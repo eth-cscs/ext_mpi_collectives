ea2a1e4f83bec8089574d9429463f06bd3acf39f
Andreas Jocksch <andreas.jocksch@cscs.ch>
2022-11-08 15:34:58 +0100

    progress data algorithm multiple

--- src/core/allreduce.c
+++ src/core/allreduce.c
@@ -92,81 +92,125 @@ static int get_num_ports_group(struct parameters_block *parameters, int step, in
   return length;
 }
 
-static int node_local(int node_global, int gbstep, int factor, int *component) {
-  *component = (node_global / factor / gbstep) * gbstep + node_global % gbstep;
-  return ((node_global / gbstep) % factor);
-}
-
-static int node_global(int node_local, int gbstep, int factor, int component) {
-  return (node_local * gbstep + component % gbstep +
-          (component / gbstep) * factor * gbstep);
-}
-
-/*static void frac_multiply(int gbstep, int igroup, int component, int fac, int *size_level0, int **size_level1, struct data_line ***data){
-  struct data_line **data_new;
-  int *size_level1_new, i, j, k, l;
-  size_level1_new = (int *) malloc(*size_level0 * sizeof(int));
-  for (i = 0; i < *size_level0; i++) {
-    size_level1_new[i] = (*size_level1)[i] * fac;
+static int socket_local(int socket_global, int step, int gbstep, int factor, int *component) {
+  if (socket_global >= 0) {
+    *component = (socket_global / factor / gbstep) * gbstep + socket_global % gbstep;
+    return (socket_global / gbstep) % factor;
+  } else {
+    return socket_global;
   }
-  data_new = (struct data_line **) malloc(*size_level0 * sizeof(struct data_line *));
-  for (j = 0; j < *size_level0; j++) {
-    data_new[j] = (struct data_line *)malloc(size_level1_new[j] * sizeof(struct data_line));
-    for (i = 0; i < (*size_level1)[j]; i++) {
+}
+
+static int socket_global(int socket_local, int step, int gbstep, int factor, int component) {
+  if (socket_local >= 0) {
+    return socket_local * gbstep + component % gbstep +
+            (component / gbstep) * factor * gbstep;
+  } else {
+    return socket_local;
+  }
+}
+
+static int expand_frac(struct parameters_block *parameters, int socket, int num_sockets, int step, int *fracs) {
+  int num_frac, i;
+  int num_ports[100], group[100];
+  if (step == 0) {
+    get_num_ports_group(parameters, step, num_ports, group);
+    for (num_frac = 0; num_frac < abs(group[0]); num_frac++) {
+      fracs[num_frac] = (num_frac + socket) % num_sockets;
+    }
+  } else if (step < 0) {
+    get_num_ports_group(parameters, step + 1, num_ports, group);
+    for (i = 0; i < abs(group[0]); i++) {
+      expand_frac(parameters, (i + socket) % abs(group[0]), abs(group[0]), step + 1, fracs + i * abs(group[0]));
+    }
+  } else {
+    get_num_ports_group(parameters, step - 1, num_ports, group);
+    for (i = 0; i < abs(group[0]); i++) {
+      expand_frac(parameters, (i + socket) % abs(group[0]), abs(group[0]), step - 1, fracs + i * abs(group[0]));
+    }
+  }
+  return num_frac;
+}
+
+static void frac_multiply(int fac, int *fracs, struct data_algorithm *data){
+  struct data_algorithm data_new;
+  int i, j, k, l;
+  data_new.num_blocks = data->num_blocks;
+  data_new.blocks = (struct data_algorithm_block *) malloc(data_new.num_blocks * sizeof(struct data_algorithm_block));
+  for (j = 0; j < data_new.num_blocks; j++) {
+    data_new.blocks[j].num_lines = data->blocks[j].num_lines * fac;
+    data_new.blocks[j].lines = (struct data_algorithm_line *)malloc(data_new.blocks[j].num_lines * sizeof(struct data_algorithm_line));
+    for (i = 0; i < data->blocks[j].num_lines; i++) {
       for (k = 0; k < fac; k++){
-        data_new[j][i * fac + k].recvfrom_max = (*data)[j][i].recvfrom_max;
-        data_new[j][i * fac + k].recvfrom_node = (int *) malloc(data_new[j][i * fac + k].recvfrom_max * sizeof(int));
-        data_new[j][i * fac + k].recvfrom_line = (int *) malloc(data_new[j][i * fac + k].recvfrom_max * sizeof(int));
-        data_new[j][i * fac + k].sendto_max = (*data)[j][i].sendto_max;
-        data_new[j][i * fac + k].sendto = (int *) malloc(data_new[j][i * fac + k].sendto_max * sizeof(int));
-        data_new[j][i * fac + k].frac = (*data)[j][i].frac;
-        for (l = 0; l < data_new[j][i * fac + k].recvfrom_max; l++) {
-          data_new[j][i * fac + k].recvfrom_node[l] = (*data)[j][i].recvfrom_node[l];
-          if (data_new[j][i * fac + k].recvfrom_node[l] >= 0) {
-            data_new[j][i * fac + k].recvfrom_node[l] = node_global(data_new[j][i * fac + k].recvfrom_node[l], gbstep, igroup, component);
-          }
-          data_new[j][i * fac + k].recvfrom_line[l] = (*data)[j][i].recvfrom_line[l] * fac + k;
+        data_new.blocks[j].lines[i * fac + k].recvfrom_max = data->blocks[j].lines[i].recvfrom_max;
+        data_new.blocks[j].lines[i * fac + k].recvfrom_node = (int *) malloc(data_new.blocks[j].lines[i * fac + k].recvfrom_max * sizeof(int));
+        data_new.blocks[j].lines[i * fac + k].recvfrom_line = (int *) malloc(data_new.blocks[j].lines[i * fac + k].recvfrom_max * sizeof(int));
+        data_new.blocks[j].lines[i * fac + k].sendto_max = data->blocks[j].lines[i].sendto_max;
+        data_new.blocks[j].lines[i * fac + k].sendto = (int *) malloc(data_new.blocks[j].lines[i * fac + k].sendto_max * sizeof(int));
+        data_new.blocks[j].lines[i * fac + k].frac = data->blocks[j].lines[i].frac;
+        for (l = 0; l < data_new.blocks[j].lines[i * fac + k].recvfrom_max; l++) {
+          data_new.blocks[j].lines[i * fac + k].recvfrom_node[l] = data->blocks[j].lines[i].recvfrom_node[l];
+          data_new.blocks[j].lines[i * fac + k].recvfrom_line[l] = data->blocks[j].lines[i].recvfrom_line[l] * fac + k;
         }
-        for (l = 0; l < data_new[j][i * fac + k].sendto_max; l++) {
-          data_new[j][i * fac + k].sendto[l] = (*data)[j][i].sendto[l];
-          if (data_new[j][i * fac + k].sendto[l] >= 0) {
-            data_new[j][i * fac + k].sendto[l] = node_global(data_new[j][i * fac + k].sendto[l], gbstep, igroup, component);
+        for (l = 0; l < data_new.blocks[j].lines[i * fac + k].sendto_max; l++) {
+          data_new.blocks[j].lines[i * fac + k].sendto[l] = data->blocks[j].lines[i].sendto[l];
+          if (data_new.blocks[j].lines[i * fac + k].sendto[l] >= 0) {
+            data_new.blocks[j].lines[i * fac + k].sendto[l] = data_new.blocks[j].lines[i * fac + k].sendto[l];
           }
         }
       }
     }
   }
-  ext_mpi_delete_algorithm(*size_level0, *size_level1, *data);
-  *size_level1 = size_level1_new;
+  ext_mpi_delete_algorithm(*data);
   *data = data_new;
-}*/
+}
+
+static void revise_partners(int step, int num_sockets, int num_sockets_l, int component, struct data_algorithm *data) {
+  int i, j, k;
+  for (i = 0; i < data->num_blocks; i++) {
+    for (j = 0; j < data->blocks[i].num_lines; j++) {
+      for (k = 0; k < data->blocks[i].lines[j].sendto_max; k++) {
+        data->blocks[i].lines[j].sendto[k] = socket_global(data->blocks[i].lines[j].sendto[k], step, num_sockets / num_sockets_l, num_sockets_l, component);
+      }
+      for (k = 0; k < data->blocks[i].lines[j].recvfrom_max; k++) {
+        data->blocks[i].lines[j].recvfrom_node[k] = socket_global(data->blocks[i].lines[j].recvfrom_node[k], step, num_sockets / num_sockets_l, num_sockets_l, component);
+      }
+    }
+  }
+}
 
 int ext_mpi_generate_allreduce(char *buffer_in, char *buffer_out) {
-  int nbuffer_out = 0, nbuffer_in = 0, group, ngroups = 0, i, k,
-      nbuffer_out_temp = 0, nbuffer_in_temp = 0;
-  struct parameters_block *parameters = NULL, *parameters2 = NULL;
-  struct data_algorithm data_l, data;
+  int nbuffer_out = 0, nbuffer_in = 0, group, ngroups[3], i, k,
+      nbuffer_out_temp = 0, nbuffer_in_temp = 0, component;
+  struct parameters_block *parameters = NULL, *parameters_l = NULL;
+  struct data_algorithm data, *data_l;
   char *buffer_in_temp, *buffer_out_temp;
   buffer_in_temp = (char *)malloc(1000000);
   buffer_out_temp = (char *)malloc(1000000);
   nbuffer_in += i = ext_mpi_read_parameters(buffer_in + nbuffer_in, &parameters);
   if (i < 0)
     goto error;
-  ngroups = get_ngroups(parameters, -1) + get_ngroups(parameters, 0) + get_ngroups(parameters, 1);
-  ext_mpi_read_parameters(buffer_in, &parameters2);
-  get_num_ports_group(parameters, 1, parameters2->num_ports, parameters2->groups);
-  parameters2->num_sockets = abs(parameters2->groups[0]);
-  parameters2->collective_type = collective_type_allgatherv;
-  nbuffer_in_temp += ext_mpi_write_parameters(parameters2, buffer_in_temp + nbuffer_in_temp);
+  ngroups[0] = get_ngroups(parameters, -1);
+  ngroups[1] = get_ngroups(parameters, 0);
+  ngroups[2] = get_ngroups(parameters, 1);
+  data_l = (struct data_algorithm*)malloc(sizeof(struct data_algorithm)*(ngroups[0]+ngroups[1]+ngroups[2]));
+  ext_mpi_read_parameters(buffer_in, &parameters_l);
+  get_num_ports_group(parameters, 0, parameters_l->num_ports, parameters_l->groups);
+  parameters_l->num_sockets = abs(parameters_l->groups[0]);
+  parameters_l->socket = socket_local(parameters_l->socket, 0, parameters->num_sockets / parameters_l->num_sockets, parameters_l->num_sockets, &component);
+//  parameters_l->collective_type = collective_type_allgatherv;
+  nbuffer_in_temp += ext_mpi_write_parameters(parameters_l, buffer_in_temp + nbuffer_in_temp);
   ext_mpi_generate_allreduce_single(buffer_in_temp, buffer_out_temp);
-  ext_mpi_delete_parameters(parameters2); parameters2 = NULL;
-  nbuffer_out_temp += ext_mpi_read_parameters(buffer_out_temp + nbuffer_out_temp, &parameters2);
-  nbuffer_out_temp += ext_mpi_read_algorithm(buffer_out_temp + nbuffer_out_temp, &data_l, parameters2->ascii_in);
-  nbuffer_out += ext_mpi_write_parameters(parameters2, buffer_out + nbuffer_out);
-  nbuffer_out += ext_mpi_write_algorithm(data_l, buffer_out + nbuffer_out, parameters->ascii_out);
+  ext_mpi_delete_parameters(parameters_l); parameters_l = NULL;
+  nbuffer_out_temp += ext_mpi_read_parameters(buffer_out_temp + nbuffer_out_temp, &parameters_l);
+  nbuffer_out_temp += ext_mpi_read_algorithm(buffer_out_temp + nbuffer_out_temp, &data_l[ngroups[0]], parameters_l->ascii_in);
+  revise_partners(0, parameters->num_sockets, parameters_l->num_sockets, component, &data_l[ngroups[0]]);
+  nbuffer_out += ext_mpi_write_parameters(parameters_l, buffer_out + nbuffer_out);
+  nbuffer_out += ext_mpi_write_algorithm(data_l[ngroups[0]], buffer_out + nbuffer_out, parameters->ascii_out);
   nbuffer_out += ext_mpi_write_eof(buffer_out + nbuffer_out, parameters->ascii_out);
-  free(parameters2);
-  ext_mpi_delete_algorithm(data);
+  free(data_l);
+  ext_mpi_delete_parameters(parameters_l);
+//  ext_mpi_delete_algorithm(data);
   ext_mpi_delete_parameters(parameters);
   free(buffer_out_temp);
   free(buffer_in_temp);
