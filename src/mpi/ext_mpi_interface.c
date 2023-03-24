6b08565740ebb52079e9c416f6c8c492b8f60917
Andreas <andreas.jocksch@cscs.ch>
2023-03-24 14:42:23 +0100

    revised interface

--- src/mpi/ext_mpi_interface.c
+++ src/mpi/ext_mpi_interface.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <mpi.h>
 #include "constants.h"
 #include "hash_table.h"
@@ -6,32 +7,48 @@
 #include "ext_mpi.h"
 #include "ext_mpi_interface.h"
 
-static int is_initialised = 0;
+static int is_blocking = 0;
 static int comms_blocking[100];
 
 int MPI_Init(int *argc, char ***argv){
-  int i;
+  int mpi_comm_rank, var, var2, i;
+  char *c;
   MPI_Comm comm = MPI_COMM_WORLD;
   int ret = PMPI_Init(argc, argv);
   ext_mpi_hash_init();
   ext_mpi_hash_init_blocking();
-  ext_mpi_hash_insert_blocking(&comm, 0);
   EXT_MPI_Init();
-  EXT_MPI_Init_blocking_comm(MPI_COMM_WORLD, 0);
-  for (i = 0; i < sizeof(comms_blocking)/sizeof(int); i++) {
-    comms_blocking[i] = 0;
+  PMPI_Comm_rank(MPI_COMM_WORLD, &mpi_comm_rank);
+  if (mpi_comm_rank == 0) {
+    var = ((c = getenv("EXT_MPI_BLOCKING")) != NULL);
+    var2 = ((c = getenv("EXT_MPI_VERBOSE")) != NULL);
+    if (var) {
+      is_blocking = 1;
+    }
+    if (var && var2) {
+      printf("# EXT_MPI blocking\n");
+    }
   }
-  comms_blocking[0] = 1;
-  is_initialised = 1;
+  MPI_Bcast(&is_blocking, 1, MPI_INT, 0, MPI_COMM_WORLD);
+  if (is_blocking) {
+    ext_mpi_hash_insert_blocking(&comm, 0);
+    EXT_MPI_Init_blocking_comm(MPI_COMM_WORLD, 0);
+    for (i = 0; i < sizeof(comms_blocking)/sizeof(int); i++) {
+      comms_blocking[i] = 0;
+    }
+    comms_blocking[0] = 1;
+  }
   return ret;
 }
 
 int MPI_Finalize(){
-  EXT_MPI_Finalize_blocking_comm(0);
+  if (is_blocking) {
+    EXT_MPI_Finalize_blocking_comm(0);
+    ext_mpi_hash_done_blocking();
+  }
   EXT_MPI_Finalize();
-  ext_mpi_hash_done_blocking();
   ext_mpi_hash_done();
-  is_initialised = 0;
+  is_blocking = 0;
   return PMPI_Finalize();
 }
 
@@ -343,7 +360,7 @@ error:
 
 int MPI_Allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm){
   int reduction_op, lcount, i;
-  if (is_initialised && op == MPI_SUM) {
+  if (is_blocking && op == MPI_SUM) {
     if (datatype == MPI_DOUBLE) {
       reduction_op = OPCODE_REDUCE_SUM_DOUBLE;
       lcount = count * sizeof(double);
@@ -372,7 +389,7 @@ int MPI_Allreduce(const void *sendbuf, void *recvbuf, int count, MPI_Datatype da
 
 int MPI_Reduce_scatter_block(const void *sendbuf, void *recvbuf, int recvcount, MPI_Datatype datatype, MPI_Op op, MPI_Comm comm){
   int reduction_op, lcount, i;
-  if (is_initialised && op == MPI_SUM) {
+  if (is_blocking && op == MPI_SUM) {
     if (datatype == MPI_DOUBLE) {
       reduction_op = OPCODE_REDUCE_SUM_DOUBLE;
       lcount = recvcount * sizeof(double);
@@ -401,7 +418,7 @@ int MPI_Reduce_scatter_block(const void *sendbuf, void *recvbuf, int recvcount, 
 
 int MPI_Allgather(const void *sendbuf, int sendcount, MPI_Datatype sendtype, void *recvbuf, int recvcount, MPI_Datatype recvtype, MPI_Comm comm){
   int type_size, i;
-  if (is_initialised) {
+  if (is_blocking) {
     i = ext_mpi_hash_search_blocking(&comm);
     if (i >= 0) {
       MPI_Type_size(sendtype, &type_size);
@@ -433,32 +450,42 @@ static int remove_comm_from_blocking(MPI_Comm *comm){
 int MPI_Comm_dup(MPI_Comm comm, MPI_Comm *newcomm) {
   int ret;
   ret = PMPI_Comm_dup(comm, newcomm);
-  add_comm_to_blocking(newcomm);
+  if (is_blocking) {
+    add_comm_to_blocking(newcomm);
+  }
   return ret;
 }
 
 int MPI_Comm_create(MPI_Comm comm, MPI_Group group, MPI_Comm *newcomm) {
   int ret;
   ret = PMPI_Comm_create(comm, group, newcomm);
-  add_comm_to_blocking(newcomm);
+  if (is_blocking) {
+    add_comm_to_blocking(newcomm);
+  }
   return ret;
 }
 
 int MPI_Comm_split(MPI_Comm comm, int color, int key, MPI_Comm *newcomm) {
   int ret;
   ret = PMPI_Comm_split(comm, color, key, newcomm);
-  add_comm_to_blocking(newcomm);
+  if (is_blocking) {
+    add_comm_to_blocking(newcomm);
+  }
   return ret;
 }
 
 int MPI_Comm_split_type(MPI_Comm comm, int split_type, int key, MPI_Info info, MPI_Comm *newcomm) {
   int ret;
   ret = PMPI_Comm_split_type(comm, split_type, key, info, newcomm);
-  add_comm_to_blocking(newcomm);
+  if (is_blocking) {
+    add_comm_to_blocking(newcomm);
+  }
   return ret;
 }
 
 int MPI_Comm_free(MPI_Comm *comm) {
-  remove_comm_from_blocking(comm);
+  if (is_blocking) {
+    remove_comm_from_blocking(comm);
+  }
   return PMPI_Comm_free(comm);
 }
