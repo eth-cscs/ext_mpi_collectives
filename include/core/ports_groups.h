6601923472fb76e58757fea8b8dd3ae7f4634ba2
Andreas <andreas.jocksch@cscs.ch>
2023-12-18 08:55:43 +0100

    refactoring MPI_Info copyin

--- include/core/ports_groups.h
+++ include/core/ports_groups.h
@@ -7,6 +7,8 @@ extern "C"
 {
 #endif
 
+char* ext_mpi_print_copyin(int copyin_method, int *copyin_factors);
+int ext_mpi_scan_copyin(char *str, int *copyin_method, int **copyin_factors);
 char* ext_mpi_print_ports_groups(int *ports, int *groups);
 int ext_mpi_scan_ports_groups(char *str, int **ports, int **groups);
 
