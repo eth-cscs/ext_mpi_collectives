668c58ea1333a2dc5b7feae9eee0cf678e11cbd5
ajocksch <andreas.jocksch@cscs.ch>
2024-03-15 16:10:51 +0100

    bug fix

--- include/core/reduce_copyin.h
+++ include/core/reduce_copyin.h
@@ -12,7 +12,6 @@ int ext_mpi_generate_allreduce_copyin(char *buffer_in, char *buffer_out);
 int ext_mpi_generate_allreduce_copyout(char *buffer_in, char *buffer_out);
 int ext_mpi_generate_allreduce_copyin_shmem(char *buffer_in, char *buffer_out);
 int ext_mpi_generate_allreduce_copyout_shmem(char *buffer_in, char *buffer_out);
-void ext_mpi_rank_order(int size, int num_factors, int *factors, int *ranks);
 
 #ifdef __cplusplus
 }
