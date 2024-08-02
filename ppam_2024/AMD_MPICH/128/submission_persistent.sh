00f9b06f2870a22fe81f86164896aaf9825907f7
ajocksch <andreas.jocksch@cscs.ch>
2024-08-02 18:04:38 +0200

    --mpi=pmi2 srun option

--- ppam_2024/AMD_MPICH/128/submission_persistent.sh
+++ ppam_2024/AMD_MPICH/128/submission_persistent.sh
@@ -5,5 +5,5 @@ export EXT_MPI_COPYIN='6;-64 -16 -4 -2 2 4 16'
 
 for i in $(seq 1 10);
 do
-  salloc --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_mpich/bin/mpirun -n 128 ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_mpich_persistent_128_$i.txt
+  srun --cpu_bind=rank --mpi=pmi2 --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_mpich_persistent_128_$i.txt
 done
--- ppam_2024/AMD_ext_mpi/128/submission.sh
+++ ppam_2024/AMD_ext_mpi/128/submission.sh
@@ -5,26 +5,26 @@ export EXT_MPI_COPYIN='6;-64 -16 -4 -2 2 4 16'
 
 for i in $(seq 1 10);
 do
-  salloc --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_mpich/bin/mpirun -n 128 ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_1_$i.txt
+  srun --cpu_bind=rank --mpi=pmi2 --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_1_$i.txt
 done
 
 export EXT_MPI_COPYIN='6;1 -16 -4 -2 2 4 16'
 
 for i in $(seq 1 10);
 do
-  salloc --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_mpich/bin/mpirun -n 128 ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_2_$i.txt
+  srun --cpu_bind=rank --mpi=pmi2 --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_2_$i.txt
 done
 
 export EXT_MPI_COPYIN='6;1 -4 -4 -4 -2 2 4 4 4'
 
 for i in $(seq 1 10);
 do
-  salloc --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_mpich/bin/mpirun -n 128 ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_3_$i.txt
+  srun --cpu_bind=rank --mpi=pmi2 --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_3_$i.txt
 done
 
 export EXT_MPI_COPYIN='6;1 -2 -2 -2 -2 -2 -2 -2 2 2 2 2 2 2 2'
 
 for i in $(seq 1 10);
 do
-  salloc --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_mpich/bin/mpirun -n 128 ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_4_$i.txt
+  srun --cpu_bind=rank --mpi=pmi2 --nodes=1 --ntasks-per-node=128 --account=csstaff --constraint=mc --partition=normal ./osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_ext_mpi_128_4_$i.txt
 done
