ff0dbdd54675a78b225c4cf1e324126b999ad581
ajocksch <andreas.jocksch@cscs.ch>
2024-08-06 16:01:10 +0200

    revised ppam 2024 submission scripts

--- ppam_2024/AMD_MPICH/127/submission_persistent.sh
+++ ppam_2024/AMD_MPICH/127/submission_persistent.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export EXT_MPI_VERBOSE=1
-export EXT_MPI_COPYIN='6;-64 -16 -4 -2 2 4 16'
-
 for i in $(seq 1 10);
 do
   srun --cpu_bind=rank --mpi=pmi2 --nodes=1 --ntasks-per-node=127 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_osu/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce_persistent -m 8:67108864 -T mpi_float | tee pilatus_mpich_persistent_127_$i.txt
--- ppam_2024/AMD_OpenMPI/127/submission_blocking.sh
+++ ppam_2024/AMD_OpenMPI/127/submission_blocking.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-export EXT_MPI_VERBOSE=1
-export EXT_MPI_COPYIN='6;-64 -16 -4 -2 2 4 16'
+# salloc --nodes=1 --ntasks-per-node=127 --account=csstaff --constraint=mc --partition=normal
+# create hostfile
 
 for i in $(seq 1 10);
 do
-  salloc --nodes=1 --ntasks-per-node=127 --account=csstaff --constraint=mc --partition=normal /capstor/scratch/cscs/ajocksch/install_openmpi/bin/mpirun -n 127 --bind-to CORE /capstor/scratch/cscs/ajocksch/install_osu_openmpi/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce -m 8:67108864 -T mpi_float | tee pilatus_openmpi_127_$i.txt
+  /capstor/scratch/cscs/ajocksch/install_openmpi/bin/mpirun --hostfile=hosts.txt -n 127 --bind-to CORE /capstor/scratch/cscs/ajocksch/install_osu_openmpi/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce -m 8:67108864 -T mpi_float | tee pilatus_openmpi_127_$i.txt
 done
