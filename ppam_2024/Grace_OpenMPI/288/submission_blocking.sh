532d7b9ef7ffcb71c015e9de15878d71b146896b
Andreas Jocksch <ajocksch@todi-ln001.cscs.ch>
2024-08-09 11:44:49 +0200

    Grace OpenMPI benchmarks

---
+++ ppam_2024/Grace_OpenMPI/288/submission_blocking.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# salloc --reservation=todi --nodes=1 --ntasks-per-node=288 --account=csstaff --constraint=gpu --partition=normal
+# create hostfile
+
+for i in $(seq 1 10);
+do
+  /capstor/scratch/cscs/ajocksch/install_openmpi_todi/bin/mpirun --hostfile hosts.txt -n 288 --bind-to CORE /capstor/scratch/cscs/ajocksch/install_osu_openmpi_todi/libexec/osu-micro-benchmarks/mpi/collective/osu_allreduce -m 8:67108864 -T mpi_float | tee todi_openmpi_288_$i.txt
+done
--- ppam_2024/Grace_OpenMPI/configure_openmpi.sh
+++ ppam_2024/Grace_OpenMPI/configure_openmpi.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+uenv image repo # if you never used uenv before
+uenv image find
+uenv image pull prgenv-gnu/24.7
+uenv start --view=modules prgenv-gnu/24.7
+
+export PATH=$PATH:/user-environment/linux-sles15-neoverse_v2/gcc-13.2.0/automake-1.16.5-6vkf2u3eefhpgcg3c3kltikemtqnlvb7/bin/automake
+
 ./configure CC=gcc CXX=g++ CFLAGS='-I/user-environment/env/icon/include' LDFLAGS='-L/user-environment/env/icon/lib64' --prefix='/capstor/scratch/cscs/ajocksch/install_openmpi_todi'
 
 make -j 32 install
