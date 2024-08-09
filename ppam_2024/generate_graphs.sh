cd00454211e2c345a8c69ef725b2505a77e08ca6
Andreas Jocksch <ajocksch@todi-ln001.cscs.ch>
2024-08-09 18:05:07 +0200

    bug fix script

--- ppam_2024/generate_graphs.sh
+++ ppam_2024/generate_graphs.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
-#for tasks in 127 128
-#do
-#./average.sh AMD_MPICH/$tasks/pilatus_mpich_$tasks_* > AMD_mpich_blocking_$tasks.txt
-#./average.sh AMD_MPICH/$tasks/pilatus_mpich_persistent_$tasks_* > AMD_mpich_persistent_$tasks.txt
-#./average.sh AMD_OpenMPI/$tasks/pilatus_openmpi_$tasks_* > AMD_openmpi_blocking_$tasks.txt
-#./average.sh AMD_OpenMPI/$tasks/pilatus_openmpi_persistent_$tasks_* > AMD_openmpi_persistent_$tasks.txt
-#
-#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_1_* > AMD_ext_mpi_$tasks_1.txt
-#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_2_* > AMD_ext_mpi_$tasks_2.txt
-#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_3_* > AMD_ext_mpi_$tasks_3.txt
-#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_4_* > AMD_ext_mpi_$tasks_4.txt
-#
-#./minimum.sh AMD_ext_mpi_$tasks_1.txt AMD_ext_mpi_$tasks_2.txt > AMD_ext_mpi_$tasks.txt
-#done
+for tasks in 127 128
+do
+  ./average.sh AMD_MPICH/$tasks/pilatus_mpich_"$tasks"_* > AMD_mpich_blocking_$tasks.txt
+  ./average.sh AMD_MPICH/$tasks/pilatus_mpich_persistent_"$tasks"_* > AMD_mpich_persistent_$tasks.txt
+  ./average.sh AMD_OpenMPI/$tasks/pilatus_openmpi_"$tasks"_* > AMD_openmpi_blocking_$tasks.txt
+  ./average.sh AMD_OpenMPI/$tasks/pilatus_openmpi_persistent_"$tasks"_* > AMD_openmpi_persistent_$tasks.txt
+
+  for i in `seq 1 4`
+  do
+    ./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_"$tasks"_"$i"_* > AMD_ext_mpi_"$tasks"_$i.txt
+  done
+
+  ./minimum.sh AMD_ext_mpi_"$tasks"_1.txt AMD_ext_mpi_"$tasks"_2.txt > AMD_ext_mpi_$tasks.txt
+done
 
 for tasks in 71 72 283 288 288_l
 do
-./average.sh Grace_MPICH/$tasks/todi_mpich_$tasks_* > Grace_mpich_blocking_$tasks.txt
-./average.sh Grace_MPICH/$tasks/todi_mpich_persistent_$tasks_* > Grace_mpich_persistent_$tasks.txt
-./average.sh Grace_OpenMPI/$tasks/todi_openmpi_$tasks_* > Grace_openmpi_blocking_$tasks.txt
-./average.sh Grace_OpenMPI/$tasks/todi_openmpi_persistent_$tasks_* > Grace_openmpi_persistent_$tasks.txt
+  ./average.sh Grace_MPICH/$tasks/todi_mpich_"$tasks"_* > Grace_mpich_blocking_$tasks.txt
+  ./average.sh Grace_MPICH/$tasks/todi_mpich_persistent_"$tasks"_* > Grace_mpich_persistent_$tasks.txt
+  ./average.sh Grace_OpenMPI/$tasks/todi_openmpi_"$tasks"_* > Grace_openmpi_blocking_$tasks.txt
+  ./average.sh Grace_OpenMPI/$tasks/todi_openmpi_persistent_"$tasks"_* > Grace_openmpi_persistent_$tasks.txt
 
-./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_1_* > Grace_ext_mpi_$tasks_1.txt
-./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_2_* > Grace_ext_mpi_$tasks_2.txt
-./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_3_* > Grace_ext_mpi_$tasks_3.txt
-./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_4_* > Grace_ext_mpi_$tasks_4.txt
+  for i in `seq 1 4`
+  do
+    ./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_"$tasks"_"$i"_* > Grace_ext_mpi_"$tasks"_$i.txt
+  done
 
-./minimum.sh Grace_ext_mpi_$tasks_1.txt Grace_ext_mpi_$tasks_2.txt > Grace_ext_mpi_$tasks.txt
+  ./minimum.sh Grace_ext_mpi_"$tasks"_1.txt Grace_ext_mpi_"$tasks"_2.txt > Grace_ext_mpi_$tasks.txt
 done
