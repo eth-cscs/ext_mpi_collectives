1d6648d11df311b18ece889db847a435eef9b70d
Andreas Jocksch <ajocksch@todi-ln001.cscs.ch>
2024-08-09 17:42:57 +0200

    progress graph processing

--- ppam_2024/generate_graphs.sh
+++ ppam_2024/generate_graphs.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
 
-./average.sh AMD_MPICH/128/pilatus_mpich_128_* > AMD_mpich_blocking.txt
-./average.sh AMD_MPICH/128/pilatus_mpich_persistent_* > AMD_mpich_persistent.txt
-./average.sh AMD_OpenMPI/128/pilatus_openmpi_128_* > AMD_openmpi_blocking.txt
-./average.sh AMD_OpenMPI/128/pilatus_openmpi_persistent_* > AMD_openmpi_persistent.txt
+#for tasks in 127 128
+#do
+#./average.sh AMD_MPICH/$tasks/pilatus_mpich_$tasks_* > AMD_mpich_blocking_$tasks.txt
+#./average.sh AMD_MPICH/$tasks/pilatus_mpich_persistent_$tasks_* > AMD_mpich_persistent_$tasks.txt
+#./average.sh AMD_OpenMPI/$tasks/pilatus_openmpi_$tasks_* > AMD_openmpi_blocking_$tasks.txt
+#./average.sh AMD_OpenMPI/$tasks/pilatus_openmpi_persistent_$tasks_* > AMD_openmpi_persistent_$tasks.txt
+#
+#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_1_* > AMD_ext_mpi_$tasks_1.txt
+#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_2_* > AMD_ext_mpi_$tasks_2.txt
+#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_3_* > AMD_ext_mpi_$tasks_3.txt
+#./average.sh AMD_ext_mpi/$tasks/pilatus_ext_mpi_$tasks_4_* > AMD_ext_mpi_$tasks_4.txt
+#
+#./minimum.sh AMD_ext_mpi_$tasks_1.txt AMD_ext_mpi_$tasks_2.txt > AMD_ext_mpi_$tasks.txt
+#done
 
-./average.sh AMD_ext_mpi/128/pilatus_ext_mpi_128_1_* > AMD_ext_mpi_1.txt
-./average.sh AMD_ext_mpi/128/pilatus_ext_mpi_128_2_* > AMD_ext_mpi_2.txt
-./average.sh AMD_ext_mpi/128/pilatus_ext_mpi_128_3_* > AMD_ext_mpi_3.txt
-./average.sh AMD_ext_mpi/128/pilatus_ext_mpi_128_4_* > AMD_ext_mpi_4.txt
+for tasks in 71 72 283 288 288_l
+do
+./average.sh Grace_MPICH/$tasks/todi_mpich_$tasks_* > Grace_mpich_blocking_$tasks.txt
+./average.sh Grace_MPICH/$tasks/todi_mpich_persistent_$tasks_* > Grace_mpich_persistent_$tasks.txt
+./average.sh Grace_OpenMPI/$tasks/todi_openmpi_$tasks_* > Grace_openmpi_blocking_$tasks.txt
+./average.sh Grace_OpenMPI/$tasks/todi_openmpi_persistent_$tasks_* > Grace_openmpi_persistent_$tasks.txt
 
-./minimum.sh AMD_ext_mpi_1.txt AMD_ext_mpi_2.txt > AMD_ext_mpi.txt
+./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_1_* > Grace_ext_mpi_$tasks_1.txt
+./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_2_* > Grace_ext_mpi_$tasks_2.txt
+./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_3_* > Grace_ext_mpi_$tasks_3.txt
+./average.sh Grace_ext_mpi/$tasks/todi_ext_mpi_$tasks_4_* > Grace_ext_mpi_$tasks_4.txt
 
-./average.sh AMD_MPICH/127/pilatus_mpich_127_* > AMD_mpich_blocking_127.txt
-./average.sh AMD_MPICH/127/pilatus_mpich_persistent_* > AMD_mpich_persistent_127.txt
-./average.sh AMD_OpenMPI/127/pilatus_openmpi_127_* > AMD_openmpi_blocking_127.txt
-./average.sh AMD_OpenMPI/127/pilatus_openmpi_persistent_* > AMD_openmpi_persistent_127.txt
-
-./average.sh AMD_ext_mpi/127/pilatus_ext_mpi_127_1_* > AMD_ext_mpi_127_1.txt
-./average.sh AMD_ext_mpi/127/pilatus_ext_mpi_127_2_* > AMD_ext_mpi_127_2.txt
-./average.sh AMD_ext_mpi/127/pilatus_ext_mpi_127_3_* > AMD_ext_mpi_127_3.txt
-./average.sh AMD_ext_mpi/127/pilatus_ext_mpi_127_4_* > AMD_ext_mpi_127_4.txt
-
-./minimum.sh AMD_ext_mpi_127_1.txt AMD_ext_mpi_127_2.txt > AMD_ext_mpi_127.txt
+./minimum.sh Grace_ext_mpi_$tasks_1.txt Grace_ext_mpi_$tasks_2.txt > Grace_ext_mpi_$tasks.txt
+done
