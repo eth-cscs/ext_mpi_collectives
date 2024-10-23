ce56714ceb5c0f2437d4eb5108150057078b5402
ajocksch <andreas.jocksch@cscs.ch>
2024-10-23 09:50:34 +0200

    code cleaning use_sendbuf_recvbuf

--- src/core/use_sendbuf_recvbuf.c
+++ src/core/use_sendbuf_recvbuf.c
@@ -1,6 +1,5 @@
 #include "use_sendbuf_recvbuf.h"
 #include "read_write.h"
-#include "reduce_copyin.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,7 +7,7 @@
 int ext_mpi_generate_use_sendbuf_recvbuf(char *buffer_in, char *buffer_out) {
   struct line_memcpy_reduce data_memcpy_reduce;
   struct line_irecv_isend data_irecv_isend;
-  int buffer_in_size = 0, nbuffer_in = 0, nbuffer_out = 0, flag, flag3, big_recvbuf, buffer_socket_offset = 0, buffer_socket_size = 0, type_size, *counts, *countsa, *displsa, *ranks, *ranks_inv, lrank_row, copyin7, i, j;
+  int buffer_in_size = 0, nbuffer_in = 0, nbuffer_out = 0, flag, flag3, big_recvbuf, buffer_socket_offset = 0, buffer_socket_size = 0, copyin7, i;
   char line[1000];
   struct parameters_block *parameters;
   enum eassembler_type estring1;
@@ -17,74 +16,11 @@ int ext_mpi_generate_use_sendbuf_recvbuf(char *buffer_in, char *buffer_out) {
   for (i = 0; i < parameters->num_nodes; i++) {
     buffer_in_size += parameters->message_sizes[i];
   }
-  switch (parameters->data_type) {
-    case data_type_int:
-    case data_type_float:
-      type_size = 4;
-      break;
-    case data_type_long_int:
-    case data_type_double:
-      type_size = 8;
-      break;
-    default:
-      type_size = 1;
-  }
-  counts = (int *)malloc(parameters->num_sockets_per_node * sizeof(int));
-  ranks = (int*)malloc(parameters->socket_row_size * parameters->num_sockets_per_node * sizeof(int));
-  ranks_inv = (int*)malloc(parameters->socket_row_size * parameters->num_sockets_per_node * sizeof(int));
-  countsa = (int*)malloc(parameters->socket_row_size * parameters->num_sockets_per_node * sizeof(int));
-  displsa = (int*)malloc((parameters->socket_row_size * parameters->num_sockets_per_node + 1) * sizeof(int));
-  for (i = 0; i < parameters->socket_row_size * parameters->num_sockets_per_node; i++) {
-    ranks[i] = i;
-  }
-  lrank_row = parameters->socket_rank + parameters->socket_number * parameters->socket_row_size;
-  if (parameters->copyin_method == 6 || parameters->copyin_method == 7) {
-    for (i = 1; parameters->copyin_factors[i] < 0; i++);
-    ext_mpi_rank_order(parameters->socket_row_size * parameters->num_sockets_per_node, i - 1, parameters->copyin_factors + 1, ranks);
-    for (i = 0; i < parameters->socket_row_size * parameters->num_sockets_per_node; i++) {
-      if (ranks[i] == lrank_row) {
-        lrank_row = i;
-        break;
-      }
-    }
-  }
-  for (i = 0; i < parameters->socket_row_size * parameters->num_sockets_per_node; i++) {
-    ranks_inv[ranks[i]] = i;
-  }
-  j = 0;
-  for (i = 0; i < parameters->counts_max; i ++) {
-    j += parameters->counts[i];
-  }
-  if (parameters->copyin_factors[0] < 0) {
-    ext_mpi_sizes_displs(parameters->socket_row_size * parameters->num_sockets_per_node, j, type_size, -parameters->copyin_factors[0], countsa, displsa);
-  } else {
-    ext_mpi_sizes_displs(parameters->socket_row_size * parameters->num_sockets_per_node, j, type_size, 0, countsa, displsa);
-  }
-  for (i = 0; i < parameters->num_sockets_per_node; i++) {
-    counts[i] = 0;
-  }
-  for (j = 0; j < parameters->num_sockets_per_node; j++) {
-    for (i = 0; i < parameters->socket_row_size * parameters->num_sockets_per_node; i++) {
-      if (i / parameters->socket_row_size == j) {
-        counts[(parameters->num_sockets_per_node - j + parameters->socket_number) % parameters->num_sockets_per_node] += countsa[ranks_inv[i]];
-      }
-    }
-  }
   buffer_socket_size = parameters->counts[0];
   buffer_socket_offset = 0;
   for (i = 0; i < parameters->socket_number; i++) {
-//    buffer_socket_offset += counts[parameters->num_sockets_per_node - i];
-//    buffer_socket_offset += counts[(parameters->num_sockets_per_node + i) % parameters->num_sockets_per_node];
     buffer_socket_offset += parameters->counts[(parameters->num_sockets_per_node + i + 1) % parameters->num_sockets_per_node];
   }
-//printf("cccc %d %d %d %d %d %d %d %d %d %d %d %d %d\n", parameters->socket_number, counts[0], counts[1], counts[2], counts[3], counts[4], counts[5], counts[6], counts[7], counts[8], counts[9], counts[10], counts[11]);
-//printf("bbbb %d %d %d %d %d %d %d %d %d %d %d %d %d\n", parameters->socket_number, countsa[0], countsa[1], countsa[2], countsa[3], countsa[4], countsa[5], countsa[6], countsa[7], countsa[8], countsa[9], countsa[10], countsa[11]);
-//printf("aaaa %d %d %d %d\n", parameters->socket_number, buffer_socket_offset, buffer_socket_size, buffer_in_size);
-  free(displsa);
-  free(countsa);
-  free(ranks_inv);
-  free(ranks);
-  free(counts);
   copyin7 = parameters->copyin_method == 7 && parameters->num_sockets_per_node > 1;
   for (i = 0; parameters->num_ports[i]; i++)
     ;
