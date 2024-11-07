cfa7592229f7ae52cd04705f0540b2b39818739b
ajocksch <andreas.jocksch@cscs.ch>
2024-11-07 08:39:14 +0100

    xpmem tree 1.

--- src/mpi/ext_mpi_xpmem.c
+++ src/mpi/ext_mpi_xpmem.c
@@ -17,8 +17,17 @@
 
 extern MPI_Comm ext_mpi_COMM_WORLD_dup;
 
-long long int *ext_mpi_all_xpmem_id;
-long long int *ext_mpi_all_xpmem_id_permutated;
+static long long int *all_xpmem_id;
+static long long int *all_xpmem_id_permutated;
+
+struct xpmem_tree {
+  struct xpmem_tree *left, *right;
+  unsigned long int offset;
+  size_t size;
+  char *a;
+};
+
+static struct xpmem_tree **xpmem_tree_root;
 
 int ext_mpi_init_xpmem(MPI_Comm comm) {
   MPI_Comm xpmem_comm_node;
@@ -26,8 +35,8 @@ int ext_mpi_init_xpmem(MPI_Comm comm) {
   long long int global_xpmem_id, *all_global_xpmem_id, a;
   my_cores_per_node = ext_mpi_get_num_tasks_per_socket(comm, 1);
   all_global_xpmem_id = (long long int *)malloc(my_cores_per_node * sizeof(long long int));
-  ext_mpi_all_xpmem_id = (long long int *)malloc(my_cores_per_node * sizeof(long long int));
-  ext_mpi_all_xpmem_id_permutated = (long long int *)malloc(my_cores_per_node * sizeof(long long int));
+  all_xpmem_id = (long long int *)malloc(my_cores_per_node * sizeof(long long int));
+  all_xpmem_id_permutated = (long long int *)malloc(my_cores_per_node * sizeof(long long int));
   PMPI_Comm_size(comm, &my_mpi_size);
   PMPI_Comm_rank(comm, &my_mpi_rank);
   PMPI_Comm_split(comm, my_mpi_rank / my_cores_per_node,
@@ -42,13 +51,13 @@ int ext_mpi_init_xpmem(MPI_Comm comm) {
   PMPI_Allgather(&global_xpmem_id, 1, MPI_LONG_LONG, all_global_xpmem_id, 1, MPI_LONG_LONG, xpmem_comm_node);
   for (i = 0; i < my_cores_per_node; i++) {
     if (my_mpi_rank != i) {
-      ext_mpi_all_xpmem_id[i] = xpmem_get (all_global_xpmem_id[i], XPMEM_RDWR, XPMEM_PERMIT_MODE, NULL);
-      if (ext_mpi_all_xpmem_id[i] == -1) {
+      all_xpmem_id[i] = xpmem_get (all_global_xpmem_id[i], XPMEM_RDWR, XPMEM_PERMIT_MODE, NULL);
+      if (all_xpmem_id[i] == -1) {
         printf("error xpmem_get\n");
         exit(1);
       }
     } else {
-      ext_mpi_all_xpmem_id[i] = -1;
+      all_xpmem_id[i] = -1;
     }
   }
   if (PMPI_Comm_free(&xpmem_comm_node) != MPI_SUCCESS) {
@@ -57,24 +66,26 @@ int ext_mpi_init_xpmem(MPI_Comm comm) {
   }
   free(all_global_xpmem_id);
   for (i = 0; i < my_mpi_size; i++) {
-    ext_mpi_all_xpmem_id_permutated[i] = ext_mpi_all_xpmem_id[i];
+    all_xpmem_id_permutated[i] = all_xpmem_id[i];
   }
   for (j = 0; j < my_mpi_rank / (my_mpi_size / my_cores_per_node) * (my_mpi_size / my_cores_per_node); j++) {
-    a = ext_mpi_all_xpmem_id_permutated[0];
+    a = all_xpmem_id_permutated[0];
     for (i = 0; i < my_mpi_size - 1; i++) {
-      ext_mpi_all_xpmem_id_permutated[i] = ext_mpi_all_xpmem_id_permutated[i + 1];
+      all_xpmem_id_permutated[i] = all_xpmem_id_permutated[i + 1];
     }
-    ext_mpi_all_xpmem_id_permutated[my_mpi_size - 1] = a;
+    all_xpmem_id_permutated[my_mpi_size - 1] = a;
   }
   for (k = 0; k < my_cores_per_node; k++) {
     for (j = 0; j < my_mpi_rank % (my_mpi_size / my_cores_per_node); j++) {
-      a = ext_mpi_all_xpmem_id_permutated[(my_mpi_size / my_cores_per_node) * k];
+      a = all_xpmem_id_permutated[(my_mpi_size / my_cores_per_node) * k];
       for (i = (my_mpi_size / my_cores_per_node) * k; i < (my_mpi_size / my_cores_per_node) * (k + 1) - 1; i++) {
-        ext_mpi_all_xpmem_id_permutated[i] = ext_mpi_all_xpmem_id_permutated[i + 1];
+        all_xpmem_id_permutated[i] = all_xpmem_id_permutated[i + 1];
       }
-      ext_mpi_all_xpmem_id_permutated[(my_mpi_size / my_cores_per_node) * (k + 1) - 1] = a;
+      all_xpmem_id_permutated[(my_mpi_size / my_cores_per_node) * (k + 1) - 1] = a;
     }
   }
+  xpmem_tree_root = (struct xpmem_tree **)malloc(my_cores_per_node * sizeof(struct xpmem_tree *));
+  memset(xpmem_tree_root, 0, my_cores_per_node * sizeof(struct xpmem_tree *));
   return 0;
 }
 
@@ -101,12 +112,12 @@ int ext_mpi_sendrecvbuf_init_xpmem(MPI_Comm comm, int my_cores_per_node, int num
     size++;
   }
   for (i = 0; i < my_mpi_size; i++) {
-    if (ext_mpi_all_xpmem_id[i] != -1) {
+    if (all_xpmem_id[i] != -1) {
       addr.offset = (long int)((*sendrecvbufs)[i]);
       while (addr.offset & (PAGESIZE - 1)) {
         addr.offset--;
       }
-      addr.apid = ext_mpi_all_xpmem_id[i];
+      addr.apid = all_xpmem_id[i];
       if (addr.apid != -1 && (*sendrecvbufs)[i]) {
         a = xpmem_attach(addr, size, NULL);
         if ((long int)a == -1) {
@@ -174,6 +185,49 @@ int ext_mpi_sendrecvbuf_done_xpmem(MPI_Comm comm, int my_cores_per_node, char **
   return 0;
 }
 
+static void xpmem_tree_put(unsigned long int offset, size_t size, int j, char *a){
+  struct xpmem_tree **p;
+  p = &xpmem_tree_root[j];
+  while (*p && (*p)->offset != offset) {
+    if (offset < (*p)->offset) {
+      p = &(*p)->left;
+    } else {
+      p = &(*p)->right;
+    }
+  }
+  if (*p) {
+    if (size > (*p)->size) {
+      (*p)->offset = offset;
+      (*p)->size = size;
+      (*p)->a = a;
+    }
+  } else {
+    *p = (struct xpmem_tree *)malloc(sizeof(struct xpmem_tree));
+    (*p)->left = (*p)->right = NULL;
+    (*p)->offset = offset;
+    (*p)->size = size;
+    (*p)->a = a;
+  }
+}
+
+static int xpmem_tree_get(unsigned long int offset, size_t size, int j, char **a){
+  struct xpmem_tree *p;
+  p = xpmem_tree_root[j];
+  while (p && p->offset != offset) {
+    if (offset < p->offset) {
+      p = p->left;
+    } else {
+      p = p->right;
+    }
+  }
+  if (p && size <= p->size) {
+    *a = p->a;
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 int ext_mpi_sendrecvbuf_init_xpmem_blocking(int my_mpi_rank, int my_cores_per_node, int num_sockets, char *sendbuf, char *recvbuf, size_t size, int *mem_partners_send, int *mem_partners_recv, char ***shmem, int **shmem_node, int *counter, char **sendbufs, char **recvbufs) {
   struct xpmem_addr addr;
   int i, j;
@@ -194,36 +248,42 @@ int ext_mpi_sendrecvbuf_init_xpmem_blocking(int my_mpi_rank, int my_cores_per_no
   for (i = 0; (j = mem_partners_send[i]) >= 0; i++) {
     if (j > 0) {
       sendbufs[j] = shmem[j][0];
-      if (ext_mpi_all_xpmem_id_permutated[j] != -1) {
+      if (all_xpmem_id_permutated[j] != -1) {
         addr.offset = (unsigned long int)(sendbufs[j]);
 	addr.offset &= 0xFFFFFFFFFFFFFFFF - (PAGESIZE - 1);
-        addr.apid = ext_mpi_all_xpmem_id_permutated[j];
+        addr.apid = all_xpmem_id_permutated[j];
         if (addr.apid != -1 && sendbufs[j]) {
-          a = xpmem_attach(addr, size, NULL);
-          if ((long int)a == -1) {
-            printf("error xpmem_attach\n");
-            exit(1);
-          }
+	  if (!xpmem_tree_get(addr.offset, size, j, &a)) {
+            a = xpmem_attach(addr, size, NULL);
+            if ((long int)a == -1) {
+              printf("error xpmem_attach\n");
+              exit(1);
+            }
+            xpmem_tree_put(addr.offset, size, j, a);
+	  }
           sendbufs[j] = a + (unsigned long int)(sendbufs[j] - addr.offset);
         } else {
 	  sendbufs[j] = sendbuf;
-        }
+	}
       }
     }
   }
   for (i = 0; (j = mem_partners_recv[i]) >= 0; i++) {
     if (j > 0) {
       recvbufs[j] = shmem[j][1];
-      if (ext_mpi_all_xpmem_id_permutated[j] != -1) {
+      if (all_xpmem_id_permutated[j] != -1) {
         addr.offset = (unsigned long int)(recvbufs[j]);
 	addr.offset &= 0xFFFFFFFFFFFFFFFF - (PAGESIZE - 1);
-        addr.apid = ext_mpi_all_xpmem_id_permutated[j];
+        addr.apid = all_xpmem_id_permutated[j];
         if (addr.apid != -1 && recvbufs[j]) {
-          a = xpmem_attach(addr, size, NULL);
-          if ((long int)a == -1) {
-            printf("error xpmem_attach\n");
-            exit(1);
-          }
+	  if (!xpmem_tree_get(addr.offset, size, j, &a)) {
+            a = xpmem_attach(addr, size, NULL);
+            if ((long int)a == -1) {
+              printf("error xpmem_attach\n");
+              exit(1);
+            }
+            xpmem_tree_put(addr.offset, size, j, a);
+	  }
           recvbufs[j] = a + (unsigned long int)(recvbufs[j] - addr.offset);
         } else {
 	  recvbufs[j] = recvbuf;
@@ -237,6 +297,7 @@ int ext_mpi_sendrecvbuf_init_xpmem_blocking(int my_mpi_rank, int my_cores_per_no
 int ext_mpi_sendrecvbuf_done_xpmem_blocking(char **sendbufs, char **recvbufs, int *mem_partners_send, int *mem_partners_recv) {
   int i;
   char *addr;
+return 0;
   for (i = 0; mem_partners_recv[i] >= 0; i++) {
     if (mem_partners_recv[i] > 0) {
       addr = recvbufs[mem_partners_send[i]];
