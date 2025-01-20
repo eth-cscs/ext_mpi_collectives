7218b347857e6de2c3dc38e1f7fb3e51702f4e24
ajocksch <andreas.jocksch@cscs.ch>
2025-01-20 09:23:24 +0100

    more benchmarks

--- ppam_2024_journal/Grace_Hopper_ext_mpi/submission.sh
+++ ppam_2024_journal/Grace_Hopper_ext_mpi/submission.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#SBATCH -N 4
+#SBATCH --ntasks-per-node=16
+
 for i in `seq 1 10`
 do
 
@@ -56,11 +59,25 @@ EXT_MPI_COPYIN='6;1 -16 16' EXT_MPI_VERBOSE=1 LD_LIBRARY_PATH=/capstor/scratch/c
 
 done
 
-exit
+for i in `seq 1 10`
+do
+
+E_XT_MPI_NUM_SOCKETS_PER_NODE=4 EXT_MPI_NUM_PORTS='4(-3 3)' EXT_MPI_COPYIN='7;1 -16 16' EXT_MPI_VERBOSE=1 LD_LIBRARY_PATH=/capstor/scratch/cscs/ajocksch/ext_mpi_collectives.git/lib:$LD_LIBRARY_PATH MPICH_GPU_SUPPORT_ENABLED=1 srun --cpu-bind=mask_cpu:0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,verbose -N 4 --ntasks-per-node=16 ./mps_wrapper.sh ./osu_allreduce -d cuda -m 8:67108864 -T mpi_float -f | tee santis_gpu_4nodes_16tasks_hpe_mpi_"$i".txt
+
+done
+
 
 for i in `seq 1 10`
 do
 
-EXT_MPI_NUM_SOCKETS_PER_NODE=4 EXT_MPI_NUM_PORTS='4(-1 -1 1 1)' EXT_MPI_COPYIN='7;1 -4 -4 4 4' EXT_MPI_VERBOSE=1 LD_LIBRARY_PATH=/capstor/scratch/cscs/ajocksch/ext_mpi_collectives.git/lib:$LD_LIBRARY_PATH MPICH_GPU_SUPPORT_ENABLED=1 srun --cpu-bind=mask_cpu:0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,verbose -N 4 --ntasks-per-node=16 ./mps_wrapper.sh ./osu_allreduce_persistent -d cuda -m 8:67108864 -T mpi_float -f | tee santis_gpu_4nodes_16tasks_ext_mpi_a_"$i".txt
+E_XT_MPI_NUM_SOCKETS_PER_NODE=4 EXT_MPI_NUM_PORTS='4(-3 3)' EXT_MPI_COPYIN='7;1 -16 16' EXT_MPI_VERBOSE=1 LD_LIBRARY_PATH=/capstor/scratch/cscs/ajocksch/ext_mpi_collectives.git/lib:$LD_LIBRARY_PATH MPICH_GPU_SUPPORT_ENABLED=1 srun --cpu-bind=mask_cpu:0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,verbose -N 4 --ntasks-per-node=16 ./mps_wrapper.sh ./osu_allreduce_persistent -d cuda -m 8:67108864 -T mpi_float -f | tee santis_gpu_4nodes_16tasks_ext_mpi_a_"$i".txt
+
+done
+
+
+for i in `seq 1 10`
+do
+
+EXT_MPI_NUM_SOCKETS_PER_NODE=4 EXT_MPI_NUM_PORTS='4(-1 -1 1 1)' EXT_MPI_COPYIN='6;1 -4 -4 4 4' EXT_MPI_VERBOSE=1 LD_LIBRARY_PATH=/capstor/scratch/cscs/ajocksch/ext_mpi_collectives.git/lib:$LD_LIBRARY_PATH MPICH_GPU_SUPPORT_ENABLED=1 srun --cpu-bind=mask_cpu:0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,0xffffffffffffffffff000000000000000000000000000000000000000000000000000000,verbose -N 4 --ntasks-per-node=16 ./mps_wrapper.sh ./osu_allreduce_persistent -d cuda -m 8:67108864 -T mpi_float -f | tee santis_gpu_4nodes_16tasks_ext_mpi_b_"$i".txt
 
 done
